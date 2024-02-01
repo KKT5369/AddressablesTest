a76ed1de6ec444a4da78818dd93a165de56fdca2
KKT <korea3702@302lab.co.kr>
2024-02-01 19:09:55 +0900

    애니메이션 추가

--- Assets/Scripts/UI/UITest.cs
+++ Assets/Scripts/UI/UITest.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace UI
 {
     public class UITest : MonoBehaviour
     {
+        [SerializeField] private Animator stageAnim;
+
+        private void Awake()
+        {
+            StartCoroutine(nameof(TestCo));
+        }
+        
+        IEnumerator TestCo()
+        {
+            stageAnim.enabled = false;
+            yield return new WaitForSeconds(3f);
+            stageAnim.enabled = true;
+        }
         
     }
 }
