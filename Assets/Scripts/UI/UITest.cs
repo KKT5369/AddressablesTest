60166acc800461a38f502abb6a0cb08aaa3d4537
KKT <korea3702@302lab.co.kr>
2024-02-05 12:38:56 +0900

    UI 애니메이션 초기화 테스트

--- Assets/Scripts/UI/UITest.cs
+++ Assets/Scripts/UI/UITest.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class UITest : MonoBehaviour
     {
         [SerializeField] private Animator stageAnim;
+        private Image _imgStage;
+        private Sprite _spriteStage;
+        private Coroutine _animCo;
 
         private void Awake()
         {
-            StartCoroutine(nameof(TestCo));
+            _imgStage = stageAnim.GetComponent<Image>();
+            _spriteStage = _imgStage.sprite;
         }
-        
+
+        private void OnEnable()
+        {
+            _animCo = StartCoroutine(nameof(TestCo));
+        }
+
+        private void OnDisable()
+        {
+            if (_animCo != null)
+            {
+                StopCoroutine(_animCo);
+            }
+            
+            stageAnim.Rebind();
+            _imgStage.sprite = _spriteStage;
+        }
+
         IEnumerator TestCo()
         {
             stageAnim.enabled = false;
