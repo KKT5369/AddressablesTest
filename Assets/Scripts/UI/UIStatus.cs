59a35772f33560184aed35435796a1755a638ce1
KKT <korea3702@302lab.co.kr>
2024-02-05 18:12:29 +0900

    status 버튼이랑 UI 매칭

--- Assets/Scripts/UI/UIStatus.cs
+++ Assets/Scripts/UI/UIStatus.cs
@@ -9,23 +9,37 @@ namespace UI
     public class UIStatus : MonoBehaviour
     {
         [SerializeField] private RectTransform[] categoryList;
+        [SerializeField] private GameObject[] categoryUi;
         private RectTransform _selectCategory;
+        private GameObject _selectCategoryUI;
+        
         
         private void Awake()
         {
-            foreach (var v in categoryList)
+            SetAddListener();
+        }
+
+        void SetAddListener()
+        {
+            for (int i = 0; i < categoryList.Length; i++)
             {
+                var v = categoryList[i];
+                var go = categoryUi[i];
                 var btn = v.GetComponent<Button>();
                 btn.onClick.AddListener((() =>
                 {
                     if (_selectCategory)
                     {
                         _selectCategory.DOAnchorPosX(15, 0.5f);
+                        _selectCategoryUI.SetActive(false);
                     }
                     v.DOAnchorPosX(45f, 0.5f);
+                    go.SetActive(true);
                     _selectCategory = v;
+                    _selectCategoryUI = go;
                 }));
             }
         }
+        
     }
 }
